b932e20853e3e4b6fe871b844f429f3f9ab66246
himanshuyadav9 <68385302+himanshuyadav9@users.noreply.github.com>
2020-12-02 21:51:09 +0530

    Update MyNodeTest.java

--- com/bridgelabz/MyNodeTest.java
+++ com/bridgelabz/MyNodeTest.java
@@ -12,12 +12,13 @@ public class MyNodeTest {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(70);
-	/*	myFirstNode.setNext(mySecondNode);
+		
+		myFirstNode.setNext(mySecondNode);
 		mySecondNode.setNext(myThirdNode);
 		boolean result = myFirstNode.getNext().equals(mySecondNode) && 
 				         mySecondNode.getNext().equals(myThirdNode);
 		Assert.assertTrue(result);
-		*/
+		
 	}
 
 }
